4640acfd2cc86b04a26144534806eb95acfaf917
Suchismita Bose <133328192+psychomita@users.noreply.github.com>
2026-02-19 21:08:08 +0530

    feat: Initial Commit

---
+++ src/main/java/com/suchismitacodes/mealbox/MealboxApplication.java
@@ -0,0 +1,13 @@
+package com.suchismitacodes.mealbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suchismitacodes/mealbox/MealboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suchismitacodes.mealbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
